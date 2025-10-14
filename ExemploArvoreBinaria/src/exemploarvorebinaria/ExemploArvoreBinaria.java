19f7a2dd90ff574d57e3e366f20370fa13221584
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-10-13 22:23:11 -0300

    refactor: remove TODO comment from main method

--- ExemploArvoreBinaria/src/exemploarvorebinaria/ExemploArvoreBinaria.java
+++ ExemploArvoreBinaria/src/exemploarvorebinaria/ExemploArvoreBinaria.java
@@ -16,7 +16,6 @@ public class ExemploArvoreBinaria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
         ArvoreABB a = new ArvoreABB();
         
